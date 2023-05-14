eb76ee79613dd2ded0eacd8cb593bd6f0514834f
angeljmercado <mercadomilletangel@gmail.com>
2023-05-13 22:41:17 -0700

    Use data from data_small to return temperature for any station and date

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask("Website")
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about") 
-def about():
-    return render_template("about.html")
-
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    data = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    date = str(f"{date[:4]}-{date[4:6]}-{date[6:8]}")
+    temperature = data.loc[data["    DATE"] == date]["   TG"].squeeze() / 10
+    return {"station": station,
+             "date": date,
+             "temperature": temperature}
 
 app.run(debug=True)
